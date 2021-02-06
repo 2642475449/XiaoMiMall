a1d70fb475a650575826fccdc4fe3d83572c33d1
降蓝 <2642475449@qq.com>
2021-02-06 20:11:07 +0800

    提交文件

---
+++ mall/src/main/java/com/xiao/mall/MallApplication.java
@@ -0,0 +1,18 @@
+package com.xiao.mall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.xiao.mall")
+
+public class MallApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MallApplication.class, args);
+    }
+
+}
---
+++ mall/src/main/java/com/xiao/mall/dao/CategoryMapper.java
@@ -0,0 +1,12 @@
+package com.xiao.mall.dao;
+
+import com.xiao.mall.pojo.Category;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface CategoryMapper {
+    @Select("select * from mall_category where id = #{id}")
+    Category findById(@Param("id") Integer id);
+
+    Category queryById(Integer id);
+}
