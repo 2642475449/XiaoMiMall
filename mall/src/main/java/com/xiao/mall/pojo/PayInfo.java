484b8409f746a75d69c2b331bfcbfb0c4520ff88
降蓝 <2642475449@qq.com>
2021-02-22 14:30:56 +0800

    Java对象

--- mall/src/main/java/com/xiao/mall/pojo/PayInfo.java
+++ mall/src/main/java/com/xiao/mall/pojo/PayInfo.java
@@ -1,8 +1,11 @@
 package com.xiao.mall.pojo;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
 public class PayInfo {
     private Integer id;
 
@@ -22,75 +25,10 @@ public class PayInfo {
 
     private Date updateTime;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public Long getOrderNo() {
-        return orderNo;
-    }
-
-    public void setOrderNo(Long orderNo) {
+    public PayInfo(Long orderNo, Integer payPlatform, String platformStatus, BigDecimal payAmount) {
         this.orderNo = orderNo;
-    }
-
-    public Integer getPayPlatform() {
-        return payPlatform;
-    }
-
-    public void setPayPlatform(Integer payPlatform) {
         this.payPlatform = payPlatform;
-    }
-
-    public String getPlatformNumber() {
-        return platformNumber;
-    }
-
-    public void setPlatformNumber(String platformNumber) {
-        this.platformNumber = platformNumber == null ? null : platformNumber.trim();
-    }
-
-    public String getPlatformStatus() {
-        return platformStatus;
-    }
-
-    public void setPlatformStatus(String platformStatus) {
-        this.platformStatus = platformStatus == null ? null : platformStatus.trim();
-    }
-
-    public BigDecimal getPayAmount() {
-        return payAmount;
-    }
-
-    public void setPayAmount(BigDecimal payAmount) {
+        this.platformStatus = platformStatus;
         this.payAmount = payAmount;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
     }
 }
